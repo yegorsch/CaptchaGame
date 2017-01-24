379b094450daf7fb7abce0763e180d3614031d47
Yegor Chsherbatykh <yegor.chsherbatykg@nu.edu.kz>
2017-01-25 00:01:51 +0600

    Initial Commit

---
+++ capcha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  capcha
+//
+//  Created by Егор on 1/25/17.
+//  Copyright © 2017 Yegor's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
